90c54331d2567a77034af09ef1709afb45f3986f
ajayg51 <ajayg.ug18.cs@nitp.ac.in>
2021-06-26 02:43:12 +0530

    Initial commit

---
+++ app/src/main/java/com/ajay/justwhatsapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.ajay.justwhatsapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import static android.Manifest.permission.READ_PHONE_NUMBERS;
+import static android.Manifest.permission.READ_PHONE_STATE;
+import static android.Manifest.permission.READ_SMS;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    EditText phoneArea,countryCode;
+    Button whatsapp;
+    String phoneNo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        phoneArea=(EditText) findViewById(R.id.phoneNo);
+        countryCode=(EditText) findViewById(R.id.Ccode);
+        whatsapp=(Button) findViewById(R.id.whatsapp);
+
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(countryCode.getText().toString().length()==0){
+                    countryCode.setText("+91");
+                }
+                phoneNo=countryCode.getText().toString();
+                phoneNo=phoneNo.concat(phoneArea.getText().toString());
+                Uri uri=Uri.parse("https://wa.me/"+phoneNo);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
